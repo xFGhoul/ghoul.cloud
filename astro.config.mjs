fe9ecd1d2b6fb4c09ca08a96ed23ed913dfcd661
xFGhoul <dauntejules9@gmail.com>
2025-07-19 08:44:28 -0400

    🐛 (core): fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import solidJs from "@astrojs/solid-js";
 
-import solidJs from '@astrojs/solid-js';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  },
-  server: {
-    host: true,
-    allowedHosts: [
-      "ghoul.cloud",
-      "cdn.ghoul.cloud",
-      "localhost",
-      "127.0.0.1"
-    ]
-  },
-  vite: {
-    server: {
-      host: true,
-      allowedHosts: [
-        'ghoul.cloud',
-        'cdn.ghoul.cloud',
-        'localhost',
-        '127.0.0.1'
-      ]
-    }
-  },
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), solidJs()]
+	devToolbar: {
+		enabled: false,
+	},
+	server: {
+		host: true,
+		allowedHosts: ["ghoul.cloud", "cdn.ghoul.cloud", "localhost", "127.0.0.1"],
+	},
+	vite: {
+		server: {
+			host: true,
+			allowedHosts: [
+				"ghoul.cloud",
+				"cdn.ghoul.cloud",
+				"localhost",
+				"127.0.0.1",
+			],
+		},
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		solidJs(),
+	],
 });
