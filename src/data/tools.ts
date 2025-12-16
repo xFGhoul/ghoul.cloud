8ccf751502538192df705a5dde9f87238ee7ce01
xFGhoul <dauntejules9@gmail.com>
2025-12-16 10:41:55 -0400

    ✨ (skills): Add Clickable Links

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -1,24 +1,25 @@
 export interface Tool {
 	name: string;
 	icon: string;
+	link: string;
 }
 
 export const tools: Tool[] = [
-	{ name: "Python", icon: "/icons/python.svg" },
-	{ name: "C++", icon: "/icons/cpp.svg" },
-	{ name: "TypeScript", icon: "/icons/typescript.svg" },
-	{ name: "Rust", icon: "/icons/rust.svg" },
-	{ name: "Astro", icon: "/icons/astro.svg" },
-	{ name: "React", icon: "/icons/react.svg" },
-	{ name: "PostgreSQL", icon: "/icons/postgresql.svg" },
-	{ name: "Redis", icon: "/icons/redis.svg" },
-	{ name: "Linux", icon: "/icons/linux.svg" },
-	{ name: "Tailwind", icon: "/icons/tailwindcss.svg" },
-	{ name: "Docker", icon: "/icons/docker.svg" },
-	{ name: "Digital Ocean", icon: "/icons/digitalocean.svg" },
-	{ name: "Github", icon: "/icons/github.svg" },
-	{ name: "Git", icon: "/icons/git.svg" },
-	{ name: "Windows", icon: "/icons/windows.svg" },
-	{ name: "Visual Studio Code", icon: "/icons/vscode-insiders.svg" },
-	{ name: "Biome", icon: "/icons/biomejs.svg" },
+	{ name: "Python", icon: "/icons/python.svg", link: "https://www.python.org/"},
+	{ name: "C++", icon: "/icons/cpp.svg", link: "https://cplusplus.com/" },
+	{ name: "TypeScript", icon: "/icons/typescript.svg", link: "https://www.typescriptlang.org/" },
+	{ name: "Rust", icon: "/icons/rust.svg", link: "https://rust-lang.org" },
+	{ name: "Astro", icon: "/icons/astro.svg", link: "https://astro.build/" },
+	{ name: "React", icon: "/icons/react.svg", link: "https://react.dev/" },
+	{ name: "PostgreSQL", icon: "/icons/postgresql.svg", link: "https://www.postgresql.org/" },
+	{ name: "Redis", icon: "/icons/redis.svg", link: "https://redis.io/" },
+	{ name: "Linux", icon: "/icons/linux.svg", link: "https://www.linux.org/" },
+	{ name: "Tailwind CSS", icon: "/icons/tailwindcss.svg", link: "https://tailwindcss.com/" },
+	{ name: "Docker", icon: "/icons/docker.svg", link: "https://www.docker.com/" },
+	{ name: "Digital Ocean", icon: "/icons/digitalocean.svg", link: "https://www.digitalocean.com/" },
+	{ name: "Github", icon: "/icons/github.svg", link: "https://github.com" },
+	{ name: "Git", icon: "/icons/git.svg", link: "https://git-scm.com" },
+	{ name: "Windows", icon: "/icons/windows.svg", link: "https://www.microsoft.com/en-us/windows/windows-11?form=MA149L" },
+	{ name: "Visual Studio Code", icon: "/icons/vscode-insiders.svg", link: "https://code.visualstudio.com/" },
+	{ name: "Biome", icon: "/icons/biomejs.svg", link: "https://biomejs.dev/" },
 ];
