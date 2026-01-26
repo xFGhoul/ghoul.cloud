dba0c210fbdf918b7833eb06a67c9d716fb191ba
xFGhoul <dauntejules9@gmail.com>
2026-01-26 09:41:48 -0800

    Support Animated Avatar

--- src/data/discord.ts
+++ src/data/discord.ts
@@ -12,11 +12,21 @@ export const getPfpUrl = (
 	discriminator?: string,
 	avatar?: string | null,
 ): string => {
+	let extension = "png";
+
+	if (avatar?.startsWith("a_")) {
+		extension = "gif";
+	}
+
 	if (avatar && avatar !== null) {
-		return `${config.discord.urls.avatar}/${id}/${avatar}.png`;
+		return `${config.discord.urls.avatar}/${id}/${avatar}.${extension}`;
 	}
 	if (discriminator && discriminator !== null && discriminator !== "0") {
 		return `${config.discord.urls.default}/${parseInt(discriminator, 10) % 5}.png`;
 	}
 	return `${config.discord.urls.default}/${id.charCodeAt(id.length - 1) % 5}.png`;
 };
+
+export const getBanner = () => {
+	return `https://dcdn.dstn.to/banners/${config.discord.id}`;
+}
